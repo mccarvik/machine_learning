12165298c8dc50910b1599eed5e342cad5666e28
mccarvik <mccarviks@gmail.com>
2016-12-12 18:42:08 +0000

    another update

--- 3ch_master.py
+++ 3ch_master.py
@@ -8,7 +8,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.cross_validation import train_test_split
 from sklearn.svm import SVC
-    
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
     
 def sigmoid(z):
     return 1.0 / (1.0 + np.exp(-z))
@@ -173,11 +173,72 @@ def linear_svm():
     plt.savefig(PIC_LOC + 'svm_linear_highgamma.png', dpi=300)
     # plt.show()
     
+
+def gini(p):
+    return (p)*(1 - (p)) + (1-p)*(1 - (1-p))
+  
+  
+def entropy(p):
+    return - p*np.log2(p) - (1 - p)*np.log2((1 - p))
+  
+  
+def error(p):
+    return 1 - np.max([p, 1 - p])
+  
+def dec_tree_impurity():
+    x = np.arange(0.0, 1.0, 0.01)
     
+    ent = [entropy(p) if p != 0 else None for p in x]
+    sc_ent = [e*0.5 if e else None for e in ent]
+    err = [error(i) for i in x]
+    fig = plt.figure()
+    ax = plt.subplot(111)
+    for i, lab, ls, c, in zip([ent, sc_ent, gini(x), err], 
+                            ['Entropy', 'Entropy (scaled)', 
+                             'Gini Impurity', 'Misclassification Error'],
+                            ['-', '-', '--', '-.'],
+                            ['black', 'lightgray', 'red', 'green', 'cyan']):
+      line = ax.plot(x, i, label=lab, linestyle=ls, lw=2, color=c)
+    
+    ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15),
+            ncol=3, fancybox=True, shadow=False)
+    
+    ax.axhline(y=0.5, linewidth=1, color='k', linestyle='--')
+    ax.axhline(y=1.0, linewidth=1, color='k', linestyle='--')
+    plt.ylim([0, 1.1])
+    plt.xlabel('p(i=1)')
+    plt.ylabel('Impurity Index')
+    plt.tight_layout()
+    plt.savefig(PIC_LOC + '/impurity.png', dpi=300, bbox_inches='tight')
+
+def build_dec_tree():
+    X, y = get_data_sklearn()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+    tree = DecisionTreeClassifier(criterion='entropy', max_depth=3, random_state=0)
+    tree.fit(X_train, y_train)
+    
+    X_combined = np.vstack((X_train, X_test))
+    y_combined = np.hstack((y_train, y_test))
+    plot_decision_regions(X_combined, y_combined, 
+                        classifier=tree, test_idx=range(105,150))
+    
+    plt.xlabel('petal length [cm]')
+    plt.ylabel('petal width [cm]')
+    plt.legend(loc='upper left')
+    plt.tight_layout()
+    plt.savefig(PIC_LOC + 'decision_tree_decision.png', dpi=300)
+  
+    export_graphviz(tree, 
+                  out_file='tree.dot', 
+                  feature_names=['petal length', 'petal width'])
+    
+    # execute "dot -Tpng tree.dot -o tree.png" to turn file into png file
     
     
 if __name__ == "__main__":
     # import pdb; pdb.set_trace()
     # support_vector_machines()
     # nonlinear_svm()
-    linear_svm()
+    # linear_svm()
+    # dec_tree_impurity()
+    build_dec_tree()
