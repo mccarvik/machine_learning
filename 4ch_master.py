dffac4d753c5e36f49d27687a0d74b85d17df26f
mccarvik <mccarviks@gmail.com>
2016-12-14 13:28:04 +0000

    preprocessing data

--- 4ch_master.py
+++ 4ch_master.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
 from io import StringIO
-from sklearn.preprocessing import Imputer, LabelEncoder
+from sklearn.preprocessing import Imputer, LabelEncoder, OneHotEncoder, MinMaxScaler, StandardScaler
+from sklearn.cross_validation import train_test_split
 
 def csv_data():
     csv_data = '''A,B,C,D
@@ -42,17 +43,51 @@ def cat_data():
     
     inv_size_mapping = {v: k for k, v in class_mapping.items()}
     df['classlabel'] = df['classlabel'].map(inv_size_mapping)
-    print(df)
+    # print(df)
     
     
     
     class_le = LabelEncoder()
     y = class_le.fit_transform(df['classlabel'].values)
-    print(y)
-    print(class_le.inverse_transform(y))
+    # print(y)
+    # print(class_le.inverse_transform(y))
+    
+    X = df[['color', 'size', 'price']].values
+    color_le = LabelEncoder()
+    X[:, 0] = color_le.fit_transform(X[:, 0])
+    print(X)
+    
+    ohe = OneHotEncoder(categorical_features=[0])
+    print(ohe.fit_transform(X).toarray())
+    print(pd.get_dummies(df[['price', 'color', 'size']]))
  
+def part_dataset():
+    df_wine = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data', header=None)
+    
+    df_wine.columns = ['Class label', 'Alcohol', 'Malic acid', 'Ash', 
+    'Alcalinity of ash', 'Magnesium', 'Total phenols', 
+    'Flavanoids', 'Nonflavanoid phenols', 'Proanthocyanins', 
+    'Color intensity', 'Hue', 'OD280/OD315 of diluted wines', 'Proline']
+    print(df_wine.head())
+    
+    X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
+    X_train, X_test, y_train, y_test = \
+          train_test_split(X, y, test_size=0.3, random_state=0)
+    
+    # Normalization of the data
+    mms = MinMaxScaler()
+    X_train_norm = mms.fit_transform(X_train)
+    X_test_norm = mms.transform(X_test)
+    
+    # Standardization of the data
+    stdsc = StandardScaler()
+    X_train_std = stdsc.fit_transform(X_train)
+    X_test_std = stdsc.transform(X_test)
+    
+    
 
 if __name__ == "__main__":
     # import pdb; pdb.set_trace()
     # csv_data()
-    cat_data()
+    # cat_data()
+    part_dataset()
