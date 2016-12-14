a9049d4b5906fcdebd87142e6548a27226f50e9e
mccarvik <mccarviks@gmail.com>
2016-12-14 16:52:10 +0000

    preprocessing data

--- 4ch_master.py
+++ 4ch_master.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
 from io import StringIO
 from sklearn.preprocessing import Imputer, LabelEncoder, OneHotEncoder, MinMaxScaler, StandardScaler
 from sklearn.cross_validation import train_test_split
+from sklearn.linear_model import LogisticRegression
+from helpers import PL4
+
 
 def csv_data():
     csv_data = '''A,B,C,D
@@ -68,7 +74,7 @@ def part_dataset():
     'Alcalinity of ash', 'Magnesium', 'Total phenols', 
     'Flavanoids', 'Nonflavanoid phenols', 'Proanthocyanins', 
     'Color intensity', 'Hue', 'OD280/OD315 of diluted wines', 'Proline']
-    print(df_wine.head())
+    # print(df_wine.head())
     
     X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
     X_train, X_test, y_train, y_test = \
@@ -84,7 +90,46 @@ def part_dataset():
     X_train_std = stdsc.fit_transform(X_train)
     X_test_std = stdsc.transform(X_test)
     
+    lr = LogisticRegression(penalty='l1', C=0.1)
+    lr.fit(X_train_std, y_train)
+    # print('Training accuracy:', lr.score(X_train_std, y_train))
+    # print('Test accuracy:', lr.score(X_test_std, y_test))
+    # print(lr.intercept_)
+    # print(lr.coef_)
     
+    fig = plt.figure()
+    ax = plt.subplot(111)
+      
+    colors = ['blue', 'green', 'red', 'cyan', 
+           'magenta', 'yellow', 'black', 
+            'pink', 'lightgreen', 'lightblue', 
+            'gray', 'indigo', 'orange']
+    
+    weights, params = [], []
+    for c in np.arange(-4, 6):
+      lr = LogisticRegression(penalty='l1', C=10**c, random_state=0)
+      lr.fit(X_train_std, y_train)
+      weights.append(lr.coef_[1])
+      params.append(10**c)
+    
+    weights = np.array(weights)
+    
+    for column, color in zip(range(weights.shape[1]), colors):
+        plt.plot(params, weights[:, column],
+               label=df_wine.columns[column+1],
+               color=color)
+    plt.axhline(0, color='black', linestyle='--', linewidth=3)
+    plt.xlim([10**(-5), 10**5])
+    plt.ylabel('weight coefficient')
+    plt.xlabel('C')
+    plt.xscale('log')
+    plt.legend(loc='upper left')
+    ax.legend(loc='upper center', 
+            bbox_to_anchor=(1.38, 1.03),
+            ncol=1, fancybox=True)
+    plt.savefig(PL4 + 'l1_path.png', dpi=300)
+    # plt.show()
+
 
 if __name__ == "__main__":
     # import pdb; pdb.set_trace()
