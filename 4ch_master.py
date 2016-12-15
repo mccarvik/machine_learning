ee53e65532b3d4ba1e8e04681d14988f8a816493
mccarvik <mccarviks@gmail.com>
2016-12-15 01:57:29 +0000

    finished ch4, need to look at it some more

--- 4ch_master.py
+++ 4ch_master.py
@@ -7,7 +7,12 @@ from io import StringIO
 from sklearn.preprocessing import Imputer, LabelEncoder, OneHotEncoder, MinMaxScaler, StandardScaler
 from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
 from helpers import PL4
+from sklearn.base import clone
+from itertools import combinations
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
 
 
 def csv_data():
@@ -130,9 +135,141 @@ def part_dataset():
     plt.savefig(PL4 + 'l1_path.png', dpi=300)
     # plt.show()
 
+class SBS():
+    def __init__(self, estimator, k_features, scoring=accuracy_score,
+               test_size=0.25, random_state=1):
+      self.scoring = scoring
+      self.estimator = clone(estimator)
+      self.k_features = k_features
+      self.test_size = test_size
+      self.random_state = random_state
+    
+    def fit(self, X, y):
+        X_train, X_test, y_train, y_test = \
+              train_test_split(X, y, test_size=self.test_size, 
+                               random_state=self.random_state)
+    
+        dim = X_train.shape[1]
+        self.indices_ = tuple(range(dim))
+        self.subsets_ = [self.indices_]
+        score = self._calc_score(X_train, y_train, 
+                               X_test, y_test, self.indices_)
+        self.scores_ = [score]
+    
+        while dim > self.k_features:
+            scores = []
+            subsets = []
+    
+            for p in combinations(self.indices_, r=dim-1):
+                score = self._calc_score(X_train, y_train, 
+                                       X_test, y_test, p)
+                scores.append(score)
+                subsets.append(p)
+    
+            best = np.argmax(scores)
+            self.indices_ = subsets[best]
+            self.subsets_.append(self.indices_)
+            dim -= 1
+    
+            self.scores_.append(scores[best])
+        self.k_score_ = self.scores_[-1]
+        return self
+    
+    def transform(self, X):
+        return X[:, self.indices_]
+    
+    def _calc_score(self, X_train, y_train, X_test, y_test, indices):
+        self.estimator.fit(X_train[:, indices], y_train)
+        y_pred = self.estimator.predict(X_test[:, indices])
+        score = self.scoring(y_test, y_pred)
+        return score
+
+def knn_sbs():
+    df_wine = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data', header=None)
+    
+    df_wine.columns = ['Class label', 'Alcohol', 'Malic acid', 'Ash', 
+    'Alcalinity of ash', 'Magnesium', 'Total phenols', 
+    'Flavanoids', 'Nonflavanoid phenols', 'Proanthocyanins', 
+    'Color intensity', 'Hue', 'OD280/OD315 of diluted wines', 'Proline']
+    
+    X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
+    X_train, X_test, y_train, y_test = \
+          train_test_split(X, y, test_size=0.3, random_state=0)
+    # Standardization of the data
+    stdsc = StandardScaler()
+    X_train_std = stdsc.fit_transform(X_train)
+    X_test_std = stdsc.transform(X_test)
+    
+    knn = KNeighborsClassifier(n_neighbors=2)
+    # selecting features
+    sbs = SBS(knn, k_features=1)
+    sbs.fit(X_train_std, y_train)
+    
+    # plotting performance of feature subsets
+    k_feat = [len(k) for k in sbs.subsets_]
+    plt.plot(k_feat, sbs.scores_, marker='o')
+    plt.ylim([0.7, 1.1])
+    plt.ylabel('Accuracy')
+    plt.xlabel('Number of features')
+    plt.grid()
+    plt.tight_layout()
+    plt.savefig(PL4 + 'sbs.png', dpi=300)
+    
+    k5 = list(sbs.subsets_[8])
+    print(df_wine.columns[1:][k5])
+    
+    knn.fit(X_train_std, y_train)
+    print('Training accuracy:', knn.score(X_train_std, y_train))
+    print('Test accuracy:', knn.score(X_test_std, y_test))
+    
+    knn.fit(X_train_std[:, k5], y_train)
+    print('Training accuracy:', knn.score(X_train_std[:, k5], y_train))
+    print('Test accuracy:', knn.score(X_test_std[:, k5], y_test))
+
+def random_forest_classifer():
+    df_wine = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data', header=None)
+    
+    df_wine.columns = ['Class label', 'Alcohol', 'Malic acid', 'Ash', 
+    'Alcalinity of ash', 'Magnesium', 'Total phenols', 
+    'Flavanoids', 'Nonflavanoid phenols', 'Proanthocyanins', 
+    'Color intensity', 'Hue', 'OD280/OD315 of diluted wines', 'Proline']
+    
+    X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
+    X_train, X_test, y_train, y_test = \
+          train_test_split(X, y, test_size=0.3, random_state=0)
+    
+    feat_labels = df_wine.columns[1:]
+    forest = RandomForestClassifier(n_estimators=10000,
+                                  random_state=0,
+                                  n_jobs=-1)
+    forest.fit(X_train, y_train)
+    importances = forest.feature_importances_
+    indices = np.argsort(importances)[::-1]
+    
+    for f in range(X_train.shape[1]):
+        print("%2d) %-*s %f" % (f + 1, 30, 
+            feat_labels[indices[f]], 
+            importances[indices[f]]))
+                    
+    plt.title('Feature Importances')
+    plt.bar(range(X_train.shape[1]), 
+                importances[indices],
+                color='lightblue', 
+                align='center')
+  
+    plt.xticks(range(X_train.shape[1]), 
+             feat_labels[indices], rotation=90)
+    plt.xlim([-1, X_train.shape[1]])
+    plt.tight_layout()
+    plt.savefig(PL4 + 'random_forest.png', dpi=300)
+    
+    X_selected = forest.transform(X_train, threshold=0.15)
+    print(X_selected.shape)
 
 if __name__ == "__main__":
     # import pdb; pdb.set_trace()
     # csv_data()
     # cat_data()
-    part_dataset()
+    # part_dataset()
+    # knn_sbs()
+    random_forest_classifer()
