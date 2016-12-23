068210c85e728003a9243a3abb4f519d6242c726
mccarvik <mccarviks@gmail.com>
2016-12-23 17:42:12 +0000

    bagging

--- 7ch_master.py
+++ 7ch_master.py
@@ -17,8 +17,9 @@ from sklearn.cross_validation import train_test_split, cross_val_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import roc_curve, auc
+from sklearn.metrics import roc_curve, auc, accuracy_score 
 from sklearn.grid_search import GridSearchCV
+from sklearn.ensemble import BaggingClassifier    
 
 def ensemble_error(n_classifier, error):
     k_start = math.ceil(n_classifier / 2.0)
@@ -303,8 +304,90 @@ def majority_vote():
         print("%0.3f+/-%0.2f %r" % (mean_score, scores.std() / 2, params))
     print('Best parameters: %s' % grid.best_params_)
     print('Accuracy: %.2f' % grid.best_score_)
+
+def bagging():
+    df_wine = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data', header=None)
+    df_wine.columns = ['Class label', 'Alcohol', 'Malic acid', 'Ash', 
+    'Alcalinity of ash', 'Magnesium', 'Total phenols', 
+    'Flavanoids', 'Nonflavanoid phenols', 'Proanthocyanins', 
+    'Color intensity', 'Hue', 'OD280/OD315 of diluted wines', 'Proline']
+    # drop 1 class
+    df_wine = df_wine[df_wine['Class label'] != 1]
+    y = df_wine['Class label'].values
+    X = df_wine[['Alcohol', 'Hue']].values
+    
+    le = LabelEncoder()
+    y = le.fit_transform(y)    
+    X_train, X_test, y_train, y_test =\
+                train_test_split(X, y, test_size=0.40, random_state=1)
+    
+    tree = DecisionTreeClassifier(criterion='entropy',     
+                                  max_depth=None,    
+                                  random_state=1)    
+    bag = BaggingClassifier(base_estimator=tree,    
+                            n_estimators=500,     
+                            max_samples=1.0,     
+                            max_features=1.0,     
+                            bootstrap=True,     
+                            bootstrap_features=False,     
+                            n_jobs=1,     
+                            random_state=1)
+    
+       
+        
+    tree = tree.fit(X_train, y_train)    
+    y_train_pred = tree.predict(X_train)    
+    y_test_pred = tree.predict(X_test)    
+        
+    tree_train = accuracy_score(y_train, y_train_pred)    
+    tree_test = accuracy_score(y_test, y_test_pred)    
+    print('Decision tree train/test accuracies %.3f/%.3f'    
+          % (tree_train, tree_test))    
+        
+    bag = bag.fit(X_train, y_train)    
+    y_train_pred = bag.predict(X_train)    
+    y_test_pred = bag.predict(X_test)    
+        
+    bag_train = accuracy_score(y_train, y_train_pred)     
+    bag_test = accuracy_score(y_test, y_test_pred)     
+    print('Bagging train/test accuracies %.3f/%.3f'    
+          % (bag_train, bag_test))
+    
+    x_min = X_train[:, 0].min() - 1    
+    x_max = X_train[:, 0].max() + 1    
+    y_min = X_train[:, 1].min() - 1    
+    y_max = X_train[:, 1].max() + 1    
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1),    
+                         np.arange(y_min, y_max, 0.1))    
+    f, axarr = plt.subplots(nrows=1, ncols=2,     
+                            sharex='col',     
+                            sharey='row',     
+                            figsize=(8, 3))    
+        
+    for idx, clf, tt in zip([0, 1],    
+                            [tree, bag],    
+                            ['Decision Tree', 'Bagging']):    
+        clf.fit(X_train, y_train)    
+        Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])    
+        Z = Z.reshape(xx.shape)    
+        axarr[idx].contourf(xx, yy, Z, alpha=0.3)    
+        axarr[idx].scatter(X_train[y_train==0, 0],     
+                           X_train[y_train==0, 1],     
+                           c='blue', marker='^')    
+        axarr[idx].scatter(X_train[y_train==1, 0],     
+                           X_train[y_train==1, 1],     
+                           c='red', marker='o')    
+        axarr[idx].set_title(tt)    
+        
+    axarr[0].set_ylabel('Alcohol', fontsize=12)    
+    plt.text(10.2, -1.2,     
+             s='Hue',     
+             ha='center', va='center', fontsize=12)    
+    plt.tight_layout()    
+    plt.savefig(PL7 + 'voting_panel.png', bbox_inches='tight', dpi=300)   
     
 
 if __name__ == "__main__":
     # ensemble_methods()
-    majority_vote()
+    # majority_vote()
+    bagging()
